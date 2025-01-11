c8ac14cf3809934cf645bb6903858f637dbb4879
MikaDevelops <mika.virtala@gmail.com>
2025-01-12 00:58:25 +0200

    find incident start indexes

--- incidents.js
+++ incidents.js
@@ -10,10 +10,10 @@ class IncidentHandler{
             solution: null,
             related_incidents: null,
             is_related: false */
-    
-    
 
-    loadDataFromFile(){
+    async loadDataFromFile(file){
+        let textFromFile = await file.text();
+        this.#makeIncidentObject(textFromFile);
 
     }
     loadDataFromLocalStorage(){
@@ -24,5 +24,24 @@ class IncidentHandler{
     }
     saveDataToLocalStorage(){
         
+    }
+    #makeIncidentObject(incidentsText){
+        let incidentStartIndexes = this.#findStartIndexes(incidentsText);
+    }
+    #findStartIndexes(incidentsText){
+        let indexes = [];
+        function findNext(startIndex){
+            let index = incidentsText.indexOf("incident_number: ", startIndex)
+            if(index != -1){
+                indexes.push(index);
+                startIndex = index+1;
+                findNext(startIndex);
+            }
+            else{
+                return null;
+            }
+        }
+        findNext(0);
+        console.log(indexes);
     }
 }
