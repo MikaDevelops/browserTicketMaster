196fb41a2325a55662fe520ab28d5445013c6fbc
MikaDevelops <mika.virtala@gmail.com>
2025-01-11 23:50:53 +0200

    Data strucure design

---
+++ incidents.js
@@ -0,0 +1,28 @@
+class IncidentHandler{
+    
+    
+/*          incident_number: null,
+            customer_id: null,
+            customer_name: null,
+            incident_state: null,
+            subject: null,
+            description: null,
+            solution: null,
+            related_incidents: null,
+            is_related: false */
+    
+    
+
+    loadDataFromFile(){
+
+    }
+    loadDataFromLocalStorage(){
+
+    }
+    saveDataToFile(){
+
+    }
+    saveDataToLocalStorage(){
+        
+    }
+}
