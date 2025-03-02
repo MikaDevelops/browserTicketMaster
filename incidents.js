dda268f533d785b8720b1bdbcc230621f0412c66
MikaDevelops <mika.virtala@gmail.com>
2025-03-02 11:58:18 +0200

    rest of incident attributes read to memory

--- incidents.js
+++ incidents.js
@@ -1,20 +1,34 @@
+/*
+TODO: mickeys coffee and coke in the keyboard -> should take both rows to the description.
+TODO: sort the cases using customer id (sortByCustomerNumber method).
+*/
+
 class IncidentHandler{
     
-    
-/*          incident_number: null,
-            customer_id: null,
-            customer_name: null,
-            incident_state: null,
-            subject: null,
-            description: null,
-            solution: null,
-            related_incidents: null,
-            is_related: false */
+    incidents = [];
+    /*  
+        incident_number: null,
+        customer_name: null,
+        incident_state: null,
+        subject: null,
+        description: null,
+        solution: null,
+        related_incidents: null,
+        is_related: false
+        customer_id: null,
+    */
 
     async loadDataFromFile(file){
         let textFromFile = await file.text();
         this.#makeIncidentObject(textFromFile);
+        this.sortByCustomerNumber();
 
+    }
+    sortByCustomerNumber(){
+        if(!this.incidents) return null;
+        for (let i=0; i<this.incidents.length; i++){
+            console.log(this.incidents[i]);
+        }
     }
     loadDataFromLocalStorage(){
 
@@ -31,12 +45,23 @@ class IncidentHandler{
             let incidentNumStart = incidentText.indexOf(":")+1;
             let incidentNumEnd = incidentText.indexOf('\n',incidentNumStart);
             incidentObject.incident_number = incidentText.slice(incidentNumStart, incidentNumEnd).trim();
-            console.log (incidentObject);
+            let customerIdStart = incidentText.indexOf(':', incidentNumEnd)+1;
+            let customerIdEnd = incidentText.indexOf('\n', customerIdStart);
+            incidentObject.customer_id = incidentText.slice(customerIdStart, customerIdEnd).trim();
+            let customerNameStart = incidentText.indexOf(':', customerIdEnd)+1;
+            let customerNameEnd = incidentText.indexOf('\n', customerNameStart);
+            incidentObject.customer_name = incidentText.slice(customerNameStart, customerNameEnd).trim();
+            let subjectStart = incidentText.indexOf(':', customerNameEnd)+1;
+            let subjectEnd = incidentText.indexOf('\n', subjectStart);
+            incidentObject.subject = incidentText.slice(subjectStart, subjectEnd).trim();
+            let descriptionStart = incidentText.indexOf(':', subjectEnd)+1;
+            let descriptionEnd = incidentText.indexOf('\n', descriptionStart);
+            incidentObject.subject = incidentText.slice(descriptionStart, descriptionEnd).trim();
+            return incidentObject;
         }
         let incidentStartIndexes = this.#findStartIndexes(incidentsText);
         let incidentLength = incidentStartIndexes.length;
         if (incidentLength < 1) alert('No incidents in file!');
-        this.incidents = [];
         for (let i=0; i < incidentLength; i++){
             let incident;
             if (i < incidentLength-1){
@@ -47,7 +72,6 @@ class IncidentHandler{
             }
             this.incidents.push(incident);
         }
-        console.log(this.incidents);
     }
     #findStartIndexes(incidentsText){
         let indexes = [];
